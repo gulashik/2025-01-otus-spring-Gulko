3f0b2e4bb364d6036a7d65db1796a7bc3629ccf7
Максим Гулько <gulashmail@gmail.com>
2024-12-06 19:11:11 +0300

    hw10 #fix1 remove hw09

--- hw09/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.hw.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.otus.hw.models.entity.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
