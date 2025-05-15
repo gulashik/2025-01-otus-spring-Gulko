2f56c339f78e8f72ea572b1464902f8fee48b464
Максим Гулько <gulashmail@gmail.com>
2025-05-15 19:58:34 +0300

    hw16 actuator

--- hw16/src/main/java/ru/otus/hw/http_requests.md
+++ hw16/src/main/java/ru/otus/hw/http_requests.md
@@ -25,3 +25,15 @@ curl --request GET -s --url 'http://localhost:8080/api/datarest/v1/books' | jq
 clear
 curl --request GET -s --url 'http://localhost:8080/api/datarest/v1/books/search/byTitle?title=BookTitle_1' | jq
 ```
+
+# Actuator
+```shell
+clear
+curl --request GET -s --url 'http://localhost:8080/actuator/health/myCustom' | jq
+```
+
+# Logfile
+```shell
+clear
+curl --request GET -s --url 'http://localhost:8080/actuator/logfile'
+```
---
+++ hw16/src/main/java/ru/otus/hw/indicator/health/MyCustomHealthIndicator.java
@@ -0,0 +1,33 @@
+package ru.otus.hw.indicator.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class MyCustomHealthIndicator implements HealthIndicator {
+
+    private static final Random random = new Random();
+
+    @Override
+    public Health health() {
+
+        if (checkIfSystemIsUp()) {
+            return Health.up()
+                    .withDetail("message", "Система работает нормально")
+                    .withDetail("status", "OK")
+                    .build();
+        } else {
+            return Health.down()
+                    .withDetail("message", "Система недоступна")
+                    .withDetail("status", "ERROR")
+                    .build();
+        }
+    }
+    
+    private boolean checkIfSystemIsUp() {
+        return random.nextBoolean();
+    }
+}
