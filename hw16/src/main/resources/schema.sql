1eceabcfa0ec6a0b26d5a45a561e8e3c761e2e92
Максим Гулько <gulashmail@gmail.com>
2025-05-15 15:35:52 +0300

    hw16 init

---
+++ hw16/src/main/resources/schema.sql
@@ -0,0 +1,37 @@
+create table if not exists authors
+(
+    id        bigserial,
+    full_name varchar(255),
+    primary key (id)
+);
+
+create table if not exists genres
+(
+    id   bigserial,
+    name varchar(255),
+    primary key (id)
+);
+
+create table if not exists books
+(
+    id        bigserial,
+    title     varchar(255),
+    author_id bigint references authors (id) on delete cascade,
+    genre_id  bigint references genres (id) on delete cascade,
+    primary key (id)
+);
+
+create table if not exists comments
+(
+    id        bigserial,
+    text      varchar(255),
+    book_id bigint references books (id) on delete cascade,
+    primary key (id)
+);
+
+-- security пользователи
+create table users (
+    username varchar(50) not null primary key,
+    password varchar(100) not null,
+    enabled boolean not null
+);
