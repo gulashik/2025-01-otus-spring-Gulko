e9250fd1db7dbdd12da55ab3a1299dea093f3165
Максим Гулько <gulashmail@gmail.com>
2025-05-16 19:39:44 +0300

    hw16 #fix2 checkstyle

--- hw16/src/main/java/ru/otus/hw/indicator/health/MyCustomHealthIndicator.java
+++ hw16/src/main/java/ru/otus/hw/indicator/health/MyCustomHealthIndicator.java
@@ -9,7 +9,7 @@ import java.util.Random;
 @Component
 public class MyCustomHealthIndicator implements HealthIndicator {
 
-    private final Random RANDOM = new Random();
+    private final Random randomizer = new Random();
 
     @Override
     public Health health() {
@@ -28,6 +28,6 @@ public class MyCustomHealthIndicator implements HealthIndicator {
     }
     
     private boolean checkIfSystemIsUp() {
-        return RANDOM.nextBoolean();
+        return randomizer.nextBoolean();
     }
 }
